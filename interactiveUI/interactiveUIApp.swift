2bf6b5644fb991aef32e5b903be066c152ed0aff
Aanya <aanyajain712@gmail.com>
2025-07-23 15:38:32 -0500

    Initial Commit

---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by Aanya Jain on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
